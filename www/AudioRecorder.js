60f664072813fba6335b2f8ab739b5ea8441aceb
Bernatovich Anatoliy <raixur@hotmail.com>
2016-11-15 14:09:15 +0200

    Naming fixes

---
+++ www/AudioRecorder.js
@@ -0,0 +1,20 @@
+function AudioRecorder() {
+}
+
+AudioRecorder.prototype.record = function (successCallback, errorCallback, duration) {
+  cordova.exec(successCallback, errorCallback, "AudioRecorder", "record", fileDir ? [fileDir] : []);
+};
+
+AudioRecorder.prototype.stop = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "AudioRecorder", "stop", []);
+};
+
+AudioRecorder.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.audioRecorder = new AudioRecorder();
+  return window.plugins.audioRecorder;
+};
+
+cordova.addConstructor(AudioRecorderAPI.install);
