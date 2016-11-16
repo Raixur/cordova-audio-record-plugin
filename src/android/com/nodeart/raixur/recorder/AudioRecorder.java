e14f83b5d904f55b07b094ec4592ddb86ecb4ed0
SinceTV <sinse@gmail.com>
2016-11-16 18:02:38 +0200

    Changed file storage path

--- src/android/com/nodeart/raixur/recorder/AudioRecorder.java
+++ src/android/com/nodeart/raixur/recorder/AudioRecorder.java
@@ -5,11 +5,13 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+
 import android.media.MediaRecorder;
 import android.media.MediaPlayer;
 import android.media.AudioManager;
 import android.os.Environment;
 import android.content.Context;
+
 import java.util.UUID;
 import java.io.FileInputStream;
 import java.io.File;
@@ -17,67 +19,68 @@ import java.io.IOException;
 
 public class AudioRecorder extends CordovaPlugin {
 
-  private MediaRecorder myRecorder;
-  private String outputFile;
+    private MediaRecorder myRecorder;
+    private String outputFile;
 
-  @Override
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    Context context = cordova.getActivity().getApplicationContext();
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        Context context = cordova.getActivity().getApplicationContext();
 
-    System.out.println("Entered...");
-    if (action.equals("record")) {
-      System.out.println("Recording...");
-      if (args.length() >= 1) {
-        outputFile = args.getString(0);
-      } else {
-        outputFile = context.getFilesDir().getAbsoluteFile() + "/"
-          + UUID.randomUUID().toString() + ".m4a";
-      }
+        System.out.println("Entered...");
+        if (action.equals("record")) {
+            System.out.println("Recording...");
+            if (args.length() >= 1) {
+                outputFile = args.getString(0);
+            } else {
+                outputFile = UUID.randomUUID().toString();
+            }
 
-      System.out.println("Path: " + outputFile);
-      myRecorder = new MediaRecorder();
-      myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-      myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-      myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-      myRecorder.setAudioSamplingRate(44100);
-      myRecorder.setAudioChannels(1);
-      myRecorder.setAudioEncodingBitRate(32000);
-      myRecorder.setOutputFile(outputFile);
+            String outputDir = context.getFilesDir().getAbsoluteFile() + "/" + outputFile + ".m4a"
 
-      try {
-        System.out.println("Preparing...");
-        myRecorder.prepare();
-        System.out.println("Prepared for recording.");
-        myRecorder.start();
-        System.out.println("Started.");
-      } catch (final Exception e) {
-        cordova.getThreadPool().execute(new Runnable() {
-          public void run() {
-            callbackContext.error(e.getMessage());
-          }
-        });
+            System.out.println("Path: " + outputFile);
+            myRecorder = new MediaRecorder();
+            myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            myRecorder.setAudioSamplingRate(44100);
+            myRecorder.setAudioChannels(1);
+            myRecorder.setAudioEncodingBitRate(32000);
+            myRecorder.setOutputFile(outputFile);
+
+            try {
+                System.out.println("Preparing...");
+                myRecorder.prepare();
+                System.out.println("Prepared for recording.");
+                myRecorder.start();
+                System.out.println("Started.");
+            } catch (final Exception e) {
+                cordova.getThreadPool().execute(new Runnable() {
+                    public void run() {
+                        callbackContext.error(e.getMessage());
+                    }
+                });
+                return false;
+            }
+            return true;
+        }
+
+        if (action.equals("stop")) {
+            System.out.println("Stopped...");
+            stopRecord(callbackContext);
+            return true;
+        }
+
         return false;
-      }
-      return true;
     }
 
-    if (action.equals("stop")) {
-      System.out.println("Stopped...");
-      stopRecord(callbackContext);
-      return true;
+    private void stopRecord(final CallbackContext callbackContext) {
+        myRecorder.stop();
+        myRecorder.release();
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                callbackContext.success(outputFile);
+            }
+        });
     }
-
-    return false;
-  }
-
-  private void stopRecord(final CallbackContext callbackContext) {
-    myRecorder.stop();
-    myRecorder.release();
-    cordova.getThreadPool().execute(new Runnable() {
-      public void run() {
-        callbackContext.success(outputFile);
-      }
-    });
-  }
 
 }
