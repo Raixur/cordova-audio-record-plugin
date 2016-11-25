2ee30889665c9ac399f45ecc1f65e3955679512b
Bernatovich Anatoliy <raixur@gmail.com>
2016-11-25 16:22:59 +0200

    Added suppot to wav recording

--- src/android/com/nodeart/raixur/recorder/AudioRecorder.java
+++ src/android/com/nodeart/raixur/recorder/AudioRecorder.java
@@ -1,86 +1,292 @@
 package com.nodeart.raixur.recorder;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
-import org.json.JSONArray;
-import org.json.JSONException;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 
-import android.media.MediaRecorder;
-import android.media.MediaPlayer;
-import android.media.AudioManager;
-import android.os.Environment;
-import android.content.Context;
-
-import java.util.UUID;
-import java.io.FileInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
 
-public class AudioRecorder extends CordovaPlugin {
+public class AudioRecorder
+{
+    /**
+     * INITIALIZING : recorder is initializing;
+     * READY : recorder has been initialized, recorder not yet started
+     * RECORDING : recording
+     * ERROR : reconstruction needed
+     * STOPPED: reset needed
+     */
+    public enum State {INITIALIZING, READY, RECORDING, ERROR, STOPPED}
 
-    private MediaRecorder myRecorder;
-    private String outputFile;
+    // The interval in which the recorded samples are output to the file
+    private static final int TIMER_INTERVAL = 120;
 
-    @Override
-    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        Context context = cordova.getActivity().getApplicationContext();
+    private AudioRecord 	 aRecorder = null;
 
-        System.out.println("Entered...");
-        if (action.equals("record")) {
-            System.out.println("Recording...");
-            if (args.length() >= 1) {
-                outputFile = args.getString(0);
+    private String			 fPath = null;
+
+    private State			 state;
+
+    // File writer
+    private RandomAccessFile fWriter;
+    private FileChannel		 fChannel;
+
+    // Number of channels, sample rate, sample size(size in bits),
+    // buffer size, audio source, sample size
+    private short 			 nChannels;
+    private int				 sRate;
+    private short			 bSamples;
+    private int				 bufferSize;
+    private int				 aSource;
+    private int				 aFormat;
+
+    // Number of frames written to file on each output(only in uncompressed mode)
+    private int				 framePeriod;
+
+    // Buffer for output(only in uncompressed mode)
+    //private ShortBuffer		shBuffer;
+    private ByteBuffer		bBuffer;
+
+    // Number of bytes written to file after header(only in uncompressed mode)
+    // after stop() is called, this size is written to the header/data chunk in the wave file
+    private int				 payloadSize;
+
+    /**
+     *
+     * Returns the state of the recorder in a RehearsalAudioRecord.State typed object.
+     * Useful, as no exceptions are thrown.
+     *
+     * @return recorder state
+     */
+    public State getState() {
+        return state;
+    }
+
+    /**
+     * Method used for recording.
+     */
+    private AudioRecord.OnRecordPositionUpdateListener updateListener = new AudioRecord.OnRecordPositionUpdateListener()
+    {
+        @Override
+        public void onPeriodicNotification(AudioRecord recorder) {
+            aRecorder.read(bBuffer, bBuffer.capacity()); // Fill buffer
+            try {
+                bBuffer.rewind();
+                fChannel.write(bBuffer); // Write buffer to file
+                payloadSize += bBuffer.capacity();
+            } catch (IOException e) {
+                stop();
+            }
+        }
+
+        @Override
+        public void onMarkerReached(AudioRecord recorder) {  }
+    };
+
+    /**
+     * Default constructor
+     *
+     * Instantiates a new recorder, in case of compressed recording the parameters can be left as 0.
+     * In case of errors, no exception is thrown, but the state is set to ERROR
+     */
+    public AudioRecorder(int audioSource, int sampleRate, int channelConfig,
+                                  int audioFormat)
+    {
+        try {
+            if (audioFormat == AudioFormat.ENCODING_PCM_16BIT) {
+                bSamples = 16;
             } else {
-                outputFile = UUID.randomUUID().toString();
+                bSamples = 8;
             }
 
-            String outputDir = context.getExternalCacheDir().getAbsoluteFile() + "/" + outputFile + ".m4a";
+            if (channelConfig == AudioFormat.CHANNEL_IN_MONO) {
+                nChannels = 1;
+            } else {
+                nChannels = 2;
+            }
 
-            System.out.println("Path: " + outputDir);
-            myRecorder = new MediaRecorder();
-            myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-            myRecorder.setAudioSamplingRate(44100);
-            myRecorder.setAudioChannels(1);
-            myRecorder.setAudioEncodingBitRate(32000);
-            myRecorder.setOutputFile(outputDir);
+            aSource = audioSource;
+            sRate   = sampleRate;
+            aFormat = audioFormat;
+
+            framePeriod = sampleRate * TIMER_INTERVAL / 1000;
+            bufferSize = framePeriod * 2 * bSamples * nChannels / 8;
+
+            if (bufferSize < AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat)) {
+                bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+                framePeriod = bufferSize / ( 2 * bSamples * nChannels / 8 );
+            }
+
+            aRecorder = new AudioRecord(audioSource, sampleRate,
+                    channelConfig, audioFormat, bufferSize);
+
+            if (aRecorder.getState() != AudioRecord.STATE_INITIALIZED) {
+                throw new Exception("AudioRecord initialization failed");
+            }
+
+            aRecorder.setRecordPositionUpdateListener(updateListener);
+            aRecorder.setPositionNotificationPeriod(framePeriod);
+
+            fPath = null;
+            state = State.INITIALIZING;
+
+        } catch (Exception e) {
+            state = State.ERROR;
+        }
+    }
+
+    /**
+     * Sets output file path, call directly after construction/reset.
+     *
+     * @param outputFile file path
+     */
+    public void setOutputFile(String outputFile) {
+        if (state == State.INITIALIZING) {
+            fPath = outputFile;
+        }
+    }
+
+    /**
+     * Prepares the recorder for recording, in case the recorder is not in the INITIALIZING state and the file path was not set
+     * the recorder is set to the ERROR state, which makes a reconstruction necessary.
+     * In case uncompressed recording is toggled, the header of the wave file is written.
+     * In case of an exception, the state is changed to ERROR
+     */
+    public void prepare() {
+        try {
+            if (state == State.INITIALIZING) {
+                if ((aRecorder.getState() == AudioRecord.STATE_INITIALIZED) & (fPath != null)) {
+                    // write file header
+
+                    fWriter = new RandomAccessFile(fPath, "rw");
+                    fChannel= fWriter.getChannel();
+
+                    fWriter.setLength(0); // Set file length to 0, to prevent unexpected behavior in case the file already existed
+                    fWriter.writeBytes("RIFF");
+                    fWriter.writeInt(0); // Final file size not known yet, write 0
+
+                    fWriter.writeBytes("WAVE");
+                    fWriter.writeBytes("fmt ");
+                    fWriter.writeInt(Integer.reverseBytes(16)); // Sub-chunk size, 16 for PCM
+                    fWriter.writeShort(Short.reverseBytes((short) 1)); // AudioFormat, 1 for PCM
+                    fWriter.writeShort(Short.reverseBytes(nChannels));// Number of channels, 1 for mono, 2 for stereo
+                    fWriter.writeInt(Integer.reverseBytes(sRate)); // Sample rate
+                    fWriter.writeInt(Integer.reverseBytes(sRate*bSamples*nChannels/8)); // Byte rate, SampleRate*NumberOfChannels*BitsPerSample/8
+                    fWriter.writeShort(Short.reverseBytes((short)(nChannels*bSamples/8))); // Block align, NumberOfChannels*BitsPerSample/8
+                    fWriter.writeShort(Short.reverseBytes(bSamples)); // Bits per sample
+
+                    fWriter.writeBytes("data");
+                    fWriter.writeInt(0); // Data chunk size not known yet, write 0
+
+                    bBuffer = ByteBuffer.allocateDirect(framePeriod*bSamples/8*nChannels);
+                    bBuffer.order(ByteOrder.LITTLE_ENDIAN);
+                    bBuffer.rewind();
+                    //shBuffer = bBuffer.asShortBuffer();
+
+                    state = State.READY;
+                }
+                else {
+                    state = State.ERROR;
+                }
+            }
+            else {
+                release();
+                state = State.ERROR;
+            }
+        }
+        catch(Exception e)
+        {
+            state = State.ERROR;
+        }
+    }
+
+    /**
+     *  Releases the resources associated with this class, and removes the unnecessary files, when necessary
+     */
+    public void release() {
+        if (state == State.RECORDING) {
+            stop();
+        }
+        else if (state == State.READY) {
+            try {
+                fWriter.close(); // Remove prepared file
+            } catch (IOException ignored) { }
+
+            (new File(fPath)).delete();
+        }
+
+        if (aRecorder != null) {
+            aRecorder.release();
+        }
+    }
+
+    /**
+     * Resets the recorder to the INITIALIZING state, as if it was just created.
+     * In case the class was in RECORDING state, the recording is stopped.
+     * In case of exceptions the class is set to the ERROR state.
+     */
+    public void reset() {
+        try {
+            if (state != State.ERROR) {
+                release();
+                fPath = null; // Reset file path
+
+                aRecorder = new AudioRecord(aSource, sRate, nChannels+1, aFormat, bufferSize);
+
+                state = State.INITIALIZING;
+            }
+        }
+        catch (Exception e) {
+            state = State.ERROR;
+        }
+    }
+
+    /**
+     * Starts the recording, and sets the state to RECORDING.
+     * Call after prepare().
+     */
+    public void start() {
+        if (state == State.READY) {
+            payloadSize = 0;
+            aRecorder.startRecording();
+            aRecorder.read(bBuffer, bBuffer.capacity());
+            bBuffer.rewind();
+
+            state = State.RECORDING;
+        }
+        else {
+            state = State.ERROR;
+        }
+    }
+
+    /**
+     *  Stops the recording, and sets the state to STOPPED.
+     * In case of further usage, a reset is needed.
+     * Also finalizes the wave file in case of uncompressed recording.
+     */
+    public void stop() {
+        if (state == State.RECORDING) {
+            aRecorder.stop();
 
             try {
-                System.out.println("Preparing...");
-                myRecorder.prepare();
-                System.out.println("Prepared for recording.");
-                myRecorder.start();
-                System.out.println("Started.");
-            } catch (final Exception e) {
-                cordova.getThreadPool().execute(new Runnable() {
-                    public void run() {
-                        callbackContext.error(e.getMessage());
-                    }
-                });
-                return false;
+                fWriter.seek(4); // Write size to RIFF header
+                fWriter.writeInt(Integer.reverseBytes(36+payloadSize));
+
+                fWriter.seek(40); // Write size to Subchunk2Size field
+                fWriter.writeInt(Integer.reverseBytes(payloadSize));
+
+                fWriter.close();
+            } catch(IOException e) {
+                state = State.ERROR;
             }
-            return true;
+
+            state = State.STOPPED;
         }
-
-        if (action.equals("stop")) {
-            System.out.println("Stopped...");
-            stopRecord(callbackContext);
-            return true;
+        else {
+            state = State.ERROR;
         }
-
-        return false;
     }
-
-    private void stopRecord(final CallbackContext callbackContext) {
-        myRecorder.stop();
-        myRecorder.release();
-        cordova.getThreadPool().execute(new Runnable() {
-            public void run() {
-                callbackContext.success(outputFile);
-            }
-        });
-    }
-
 }
---
+++ src/android/com/nodeart/raixur/recorder/CordovaRecorder.java
@@ -0,0 +1,93 @@
+package com.nodeart.raixur.recorder;
+
+import android.content.Context;
+import android.media.AudioFormat;
+import android.media.MediaRecorder;
+import android.os.Handler;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.UUID;
+
+public class CordovaRecorder extends CordovaPlugin {
+
+    private static final int RECORDING_DEVICE = MediaRecorder.AudioSource.MIC;
+    private static final int SAMPLE_RATE = 44100;
+    private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+    private static final int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+
+    private AudioRecorder recorder;
+
+
+    private String outputPath;
+    private int duration;
+
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if(action.equals("record")) {
+            initRecord(args);
+            record(callbackContext);
+            return true;
+        }
+        else if(action.equals("startRecord")) {
+            initRecord(args);
+            startRecord();
+            return true;
+        }
+        else if(action.equals("stopRecord")) {
+            stopRecord(callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void record(CallbackContext callbackContext) {
+        recorder.start();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                recorder.release();
+            }
+        }, duration);
+
+        callbackContext.success(outputPath);
+    }
+
+    private void startRecord() {
+        recorder.start();
+    }
+
+    private void stopRecord(CallbackContext callbackContext) {
+        recorder.stop();
+        callbackContext.success(outputPath);
+    }
+
+    private void initRecord(JSONArray args) throws JSONException {
+        Context context = cordova.getActivity().getApplicationContext();
+        String outputFile;
+
+        if (args.length() >= 1) {
+            outputFile = args.getString(0);
+        } else {
+            outputFile = UUID.randomUUID().toString();
+        }
+
+        if (args.length() >= 2) {
+            duration = Integer.parseInt(args.getString(1));
+        } else {
+            duration = 0;
+        }
+
+        outputPath = context.getExternalCacheDir().getAbsoluteFile() + "/" + outputFile + ".m4a";
+
+        recorder = new AudioRecorder(RECORDING_DEVICE, SAMPLE_RATE,
+                    CHANNEL_CONFIG, AUDIO_ENCODING);
+        recorder.setOutputFile(outputPath);
+        recorder.prepare();
+    }
+}
